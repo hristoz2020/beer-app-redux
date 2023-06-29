f3b584ea86c3dac0e8b0dd751b05759fb1da6d26
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-06-29 13:52:42 +0300

    create home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import Navigation from './components/Navigation';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <Navigation />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navigation />
+			<Routes>
+				<Route path="/beer-app" element={<Home />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
