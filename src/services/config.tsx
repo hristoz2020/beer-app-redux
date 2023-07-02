0e1f35a4095cde0d2087330edae893a1ca013884
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-07-02 18:11:41 +0300

    create config file

---
+++ src/services/config.tsx
@@ -0,0 +1,7 @@
+export const BASE_URL = "https://api.punkapi.com/v2";
+export const BEERS = "/beers"
+export const RANDOM = "/random";
+export const PAGE = "?page=";
+export const PER_PAGE = "&per_page=";
+
+export const GET_RANDOM_BEER = BASE_URL + BEERS + RANDOM;
