b2eba5f4fe309444039a421f0e0058a257fa3b99
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-07-04 19:31:28 +0300

    add pagination in beers page

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,10 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Beers from "./pages/Beers";
 import Details from "./pages/Details";
 import Home from "./pages/Home";
-import { setBeers } from "./redux/slices/beersSlice";
-import { paginationBeers } from "./services/beerService";
-import { Beer } from "./types/beerTypes";
 
 function App() {
-	const dispatch = useDispatch();
-	useEffect(() => {
-		async function getBeers() {
-			let response = [];
-			for (let i = 1; i <= 5; i++) {
-				response.push(paginationBeers(i, 65));
-			}
-			response = await Promise.all(response);
-			const flattenedResult: Beer[] = response.flat();
-			dispatch(setBeers(flattenedResult));
-		}
-		getBeers();
-	}, [dispatch]);
-
 	return (
 		<div className="App">
 			<Navigation />
--- src/pages/Beers.tsx
+++ src/pages/Beers.tsx
@@ -1,21 +1,76 @@
-import { FC } from "react";
-import { useSelector } from "react-redux";
+import { useState, useEffect, ChangeEvent } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import BeerCard from "../components/BeerCard";
 import Loader from "../components/Loader";
 import { RootState } from "../redux/store";
 import { Beer } from "../types/beerTypes";
+import ReactPaginate from "react-paginate";
+import { paginationBeers } from "../services/beerService";
+import { setBeers } from "../redux/slices/beersSlice";
 
-const Beers: FC = () => {
+const Beers = () => {
+	const dispatch = useDispatch();
 	const beers = useSelector<RootState, Beer[]>((state) => state.beers.beers);
+	const [pageNumber, setPageNumber] = useState<number>(1);
+	const [beersPerPage, setBeersPerPage] = useState<number>(10);
+	const pagesVisited = pageNumber * beersPerPage;
+	const pagesCount = Math.ceil(325 / beersPerPage);
+	const displayBeers = beers
+		.slice(pagesVisited, pagesVisited + beersPerPage)
+		.map((beer: Beer) => {
+			return <BeerCard key={beer.id} beer={beer} />;
+		});
+
+	const changePage = ({ selected }: { selected: number }) => {
+		setPageNumber(selected + 1); //add 1 because in react-pagination start from 0 and in request start from 1
+	};
+
+	const handleSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
+		const selectedValue: number = Number(e.target.value);
+		setBeersPerPage(selectedValue);
+	};
+
+	useEffect(() => {
+		paginationBeers(pageNumber, beersPerPage).then((res) => {
+			const beersResponse: Beer[] = res;
+			dispatch(setBeers(beersResponse));
+		});
+	}, [beersPerPage, pageNumber, dispatch]);
 
 	return (
 		<div>
-			<h1>Beers page</h1>
 			{beers.length <= 0 && <Loader />}
 			<div className="d-flex justify-content-center flex-wrap">
 				{beers.map((beer: Beer) => (
 					<BeerCard beer={beer} key={beer.id} />
 				))}
+
+				{beers.length < 1 && <h1>No beers found!</h1>}
+				{beers.length > 0 && displayBeers}
+			</div>
+			<div className="paginationBttns d-flex flex-wrap">
+				<select
+					value={beersPerPage}
+					onChange={handleSelectChange}
+					className="mb-4"
+				>
+					<option value="10">10</option>
+					<option value="25">25</option>
+					<option value="50">50</option>
+				</select>
+				{beers.length > 0 && (
+					<ReactPaginate
+						previousLabel={"Previous"}
+						nextLabel={"Next"}
+						pageCount={pagesCount}
+						onPageChange={changePage}
+						containerClassName={"pagination justify-content-center"}
+						previousLinkClassName={"previousBttn"}
+						nextLinkClassName={"nextBttn"}
+						disabledClassName={"paginationDisabled"}
+						activeClassName={"paginationActive"}
+					/>
+				)}
 			</div>
 		</div>
 	);
