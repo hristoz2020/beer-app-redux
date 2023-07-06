d54c521f47e7a816a45aa02a129ef63ce35fd3dc
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-07-06 16:35:03 +0300

    added redux-persist

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,16 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import paginationBeersReducer from "./slices/paginationBeersSlice";
 import beersReducer from "./slices/beersSlice";
 import randomBeerReducer from "./slices/randomBeerSlice";
 import favoriteBeersReducer from "./slices/favoriteBeersSlice";
 
-export const store = configureStore({
-	reducer: {
-		beers: beersReducer,
-		paginationBeers: paginationBeersReducer,
-		randomBeer: randomBeerReducer,
-		favoriteBeers: favoriteBeersReducer,
-	},
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  beers: beersReducer,
+  paginationBeers: paginationBeersReducer,
+  randomBeer: randomBeerReducer,
+  favoriteBeers: favoriteBeersReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
