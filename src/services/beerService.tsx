0465ee89e25d8828ec88f96c0ec23fb671c92114
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-07-06 17:29:05 +0300

    changed links in requests

--- src/services/beerService.tsx
+++ src/services/beerService.tsx
@@ -1,34 +1,41 @@
-const baseUrl: string = "https://api.punkapi.com/v2";
+import {
+	BASE_URL,
+	BEERS,
+	BEER_NAME,
+	GET_BEERS,
+	GET_RANDOM_BEER,
+	PAGE,
+	PER_PAGE,
+} from "./config";
 
 export const paginationBeers = async (page: number, perPage: number) => {
 	let response = await fetch(
-		`${baseUrl}/beers?page=${page}&per_page=${perPage}`
+		BASE_URL + BEERS + PAGE + page + PER_PAGE + perPage
 	);
 
 	return response.json();
 };
 
 export const getSingleBeer = async (id: string | undefined) => {
-	let response = await fetch(`${baseUrl}/beers/${id}`);
+	let response = await fetch(BASE_URL + BEERS + id);
 
 	return response.json();
 };
 
 export const getRandomBeer = async () => {
-	let response = await fetch(`${baseUrl}/beers/random`);
+	let response = await fetch(GET_RANDOM_BEER);
 
 	return response.json();
 };
 
 export const getBeerByName = async (name: string) => {
-	let response = await fetch(`${baseUrl}/beers?beer_name=${name}`);
+	let response = await fetch(BASE_URL + BEERS + BEER_NAME + name);
 
 	return response.json();
 };
 
-
 export const getBeers = async () => {
-	let response = await fetch(`${baseUrl}/beers`);
+	let response = await fetch(GET_BEERS);
 
 	return response.json();
 };
--- src/services/config.tsx
+++ src/services/config.tsx
@@ -1,7 +1,9 @@
 export const BASE_URL = "https://api.punkapi.com/v2";
 export const BEERS = "/beers"
-export const RANDOM = "/random";
+const RANDOM = "/random";
 export const PAGE = "?page=";
 export const PER_PAGE = "&per_page=";
+export const BEER_NAME = "?beer_name=";
 
+export const GET_BEERS = BASE_URL + BEERS;
 export const GET_RANDOM_BEER = BASE_URL + BEERS + RANDOM;
